0950833bf36e14ea853c41fbba57e903ebe4c469
ImHungry48 <alaisha.barber2@gmail.com>
2023-12-04 20:07:52 -0500

    Finished GameGrid.h. Need to add code lines for SFML resources

--- include/GameGrid.h
+++ include/GameGrid.h
@@ -8,14 +8,30 @@ class GameGrid {
 public:
     GameGrid(int width, int height);
 
+    // Grid initialization
     void InitializeGrid();
 
+    // Swap functionality
+    void SwapTiles(int row1, int col1, int row2, int col2);
+
+    // Match checking and clearing
+    void FindMatches();
+    void ClearMatches();
+
+    // Grid updating
+    void UpdateGrid();
+
+    // Utility functions
+    bool IsMovePossible() const;
+    Tile& GetTile(int row, int col);
+
 private:
-    int width, height;
-    std::vector<std::vector<Tile>> grid;
+    int width, height; // Grid dimensions
+    std::vector<std::vector<Tile>> grid; // 2D grid of tiles
 
     // Helper methods
     TileType GenerateRandomType() const;
+    bool CheckForInitialMatches(int row, int col, TileType type) const;
 };
 
 #endif // GAMEGRID
--- include/Match3Game.h
+++ include/Match3Game.h
@@ -1,3 +1,32 @@
-class Match3Game {
+#ifndef MATCH3GAME_H
+#define MATCH3GAME_H
 
+#include "GameGrid.h"
+#include <SFML/Graphics.hpp>
+
+class Match3Game {
+public:
+    Match3Game(int gridWidth, int gridHeight, int tileSize);
+    void Run();
+
+private:
+    GameGrid gameGrid;
+    int tileSize;
+    sf::Vector2i offset;
+    sf:: RenderWindow window;
+
+    // TODO: SFML Resources
+
+    // Game state variables
+    int selectedRow, selectedCol;
+    bool isSwap, isMoving;
+
+    // Private methods for game logic
+    void ProcessInput();
+    void UpdateGame();
+    void Render();
+
+    
 };
+
+#endif // MATCH3GAME_H
