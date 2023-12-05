8415bf1d459668095cf296316e5de3808f169b4f
ImHungry48 <alaisha.barber2@gmail.com>
2023-12-04 20:01:06 -0500

    Added more functions to Tile.h and began functions in GameGrid.h

--- include/GameGrid.h
+++ include/GameGrid.h
@@ -1,8 +1,21 @@
 #ifndef GAMEGRID
 #define GAMEGRID
 
-class GameGrid {
+#include <vector>
+#include "Tile.h"
 
+class GameGrid {
+public:
+    GameGrid(int width, int height);
+
+    void InitializeGrid();
+
+private:
+    int width, height;
+    std::vector<std::vector<Tile>> grid;
+
+    // Helper methods
+    TileType GenerateRandomType() const;
 };
 
 #endif // GAMEGRID
--- include/Tile.h
+++ include/Tile.h
@@ -9,20 +9,42 @@ class Tile {
 public:
     // Constructors
     Tile();
-    Tile(const TileType& type);
-
+    Tile(const TileType& type, int gridRow, int gridCol);
 
     // Destructor
     ~Tile();
 
+    // Setters and Getters for type
     void SetType(TileType type);
     TileType GetType() const;
 
+    // Setters and Getters for grid position
+    void SetPosition(int row, int col);
+    int GetRow() const;
+    int GetColumn() const;
+
+    // Setters and Getters for screen positions
+    void SetScreenPosition(int x, int y);
+    int GetX() const;
+    int GetY() const;
+
+    // Matching properties
+    void SetMatch(bool match);
+    bool IsMatch() const;
+
+    // Animation properties
+    void SetAlpha(int alpha);
+    int GetAlpha() const;
+
+    // Swap functionality
     void Swap(Tile& other);
-    bool IsMatch(const Tile& other) const;
 
 private:
-    TileType type;
+    TileType type; // Piece type on tile
+    int gridRow, gridCol; // Position in the grid
+    int x, y; // Position on the screen
+    bool match; // Flag to indicate if the tile is part of a match
+    int alpha; // Alpha value for animation
 };
 
 #endif // TILE_H
